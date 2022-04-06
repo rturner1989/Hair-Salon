9b87f1c715c7346374a16c355e9114e1e3d9685f
rturner1989 <robturner1000@hotmail.co.uk>
2022-04-06 20:41:13 +0100

    Edited and simplified Data file

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -2,40 +2,61 @@ import serviceImg from "./../Images/hair-tools.webp";
 import hairTypeImg from "./../Images/redhead.webp";
 import priceImg from "./../Images/Smiling-Woman.webp";
 import toolsImg from "./../Images/woman-mixing-hair-dye-bowl.webp";
+import ladiesServiceImg from "./../Images/female-cutting.webp";
+import colorServiceImg from "./../Images/hairdresser-does-hair-style-woman.webp";
+import gentsServiceImg from "./../Images/stylish-man-sitting-barbershop.webp";
+import bridalServiceImg from "./../Images/wedding-hair.webp";
 
-export const optionLadies = [
-    { name: "Wash, Cut & Finish", price: "£32.00" },
-    { name: "Restyle", price: "£37.00" },
-    { name: "Dry Cut", price: "£20.00" },
-    { name: "Wash & Cut", price: "£25.00" },
-    { name: "Wash & Finish", price: "£15.00" },
-    { name: "Wash & Cut (thick/long)", price: "£20.00" },
-    { name: "Wash & Curl", price: "£20.00" },
-    { name: "Wash & Blast Dry", price: "£10.00" },
-];
-
-export const optionColour = [
-    { name: "Root touch up", price: "£62.00" },
-    { name: "Full Head Colour", price: "£72.00" },
-    { name: "Semi-Permanent", price: "£62.00" },
-    { name: "Full Head Foils", price: "£82.00" },
-    { name: "Half Head Foils", price: "£77.00" },
-    { name: "T-Section Foils", price: "£72.00" },
-    { name: "Parting Foils", price: "£62.00" },
-    { name: "Face Frame", price: "£52.00" },
-    { name: "Balayage", price: "£92.00" },
-    { name: "Toner", price: "£10.00" },
-];
-
-export const optionGents = [
-    { name: "Gents Cut", price: "£10.00" },
-    { name: "Head Shave", price: "£6.00" },
-    { name: "Childs Cut", price: "£7.00 - £10.00" },
-];
-
-export const optionHairUp = [
-    { name: "Hair Up Do", price: "£30.00" },
-    { name: "Bridal", price: "On Request" },
+export const services = [
+    {
+        title: "Ladies",
+        img: ladiesServiceImg,
+        options: [
+            { name: "Wash, Cut & Finish", price: "£32.00" },
+            { name: "Restyle", price: "£37.00" },
+            { name: "Dry Cut", price: "£20.00" },
+            { name: "Wash & Cut", price: "£25.00" },
+            { name: "Wash & Finish", price: "£15.00" },
+            { name: "Wash & Cut (thick/long)", price: "£20.00" },
+            { name: "Wash & Curl", price: "£20.00" },
+            { name: "Wash & Blast Dry", price: "£10.00" },
+        ],
+    },
+    {
+        title: "Colours",
+        img: colorServiceImg,
+        options: [
+            { name: "Root touch up", price: "£62.00" },
+            { name: "Full Head Colour", price: "£72.00" },
+            { name: "Semi-Permanent", price: "£62.00" },
+            { name: "Full Head Foils", price: "£82.00" },
+            { name: "Half Head Foils", price: "£77.00" },
+            { name: "T-Section Foils", price: "£72.00" },
+            { name: "Parting Foils", price: "£62.00" },
+            { name: "Face Frame", price: "£52.00" },
+            { name: "Balayage", price: "£92.00" },
+            { name: "Toner", price: "£10.00" },
+        ],
+    },
+    {
+        title: "Gents",
+        img: gentsServiceImg,
+        reverse: "reverse",
+        options: [
+            { name: "Gents Cut", price: "£10.00" },
+            { name: "Head Shave", price: "£6.00" },
+            { name: "Childs Cut", price: "£7.00 - £10.00" },
+        ],
+    },
+    {
+        title: "Hair Up",
+        img: bridalServiceImg,
+        reverse: "reverse",
+        options: [
+            { name: "Hair Up Do", price: "£30.00" },
+            { name: "Bridal", price: "On Request" },
+        ],
+    },
 ];
 
 export const homeTiles = [
