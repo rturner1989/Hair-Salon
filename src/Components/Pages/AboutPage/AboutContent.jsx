91c53eb701a5e94e82306d209f0a5bde5ca3ead2
rturner1989 <robturner1000@hotmail.co.uk>
2022-04-20 19:56:42 +0100

    Conditionally render section based on screen size

--- src/Components/Pages/AboutPage/AboutContent.jsx
+++ src/Components/Pages/AboutPage/AboutContent.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import useWindowDimensions from "../../../Hooks/useWindowDimensions";
 
 const AboutContent = ({ content }) => {
     const { img, imgAlt, title, personalQuote, indepthInfo, reverse } = content;
+
+    const [windowDimensions] = useWindowDimensions();
+
+    if (windowDimensions.width <= 430) {
+        return (
+            <section
+                className="main-content-section-mobile"
+                id={reverse ? "about-reverse-mobile" : ""}
+            >
+                <div className="sub-heading-mobile">
+                    <h2>{title}</h2>
+                </div>
+                <div className="img-quote-mobile">
+                    <div className="img-container-mobile">
+                        <img
+                            className="section-img-mobile portrait-img-mobile"
+                            src={img}
+                            alt={imgAlt}
+                        />
+                    </div>
+                    <div className="personal-quote-mobile">
+                        <p>
+                            <i>{personalQuote}</i>
+                        </p>
+                    </div>
+                </div>
+                <div className="main-content-container-mobile">
+                    <div className="indepth-info-mobile">
+                        {indepthInfo.map((info, index) => {
+                            return <p key={index}>{info}</p>;
+                        })}
+                    </div>
+                </div>
+            </section>
+        );
+    }
+
     return (
         <section
             className="main-content-section"
