c89d07dc899883b35020904178121e3de1918ce1
rturner1989 <robturner1000@hotmail.co.uk>
2022-04-22 20:41:44 +0100

    Stripped back conditional components to root

--- src/Components/Pages/ServicePage/ServiceSection.jsx
+++ src/Components/Pages/ServicePage/ServiceSection.jsx
@@ -1,37 +1,17 @@
 import React from "react";
 import ServiceTile from "./ServiceTile";
 import { services } from "../../../Data/Data";
-import useWindowDimensions from "../../../Hooks/useWindowDimensions";
-import ServiceTileMobile from "./ServiceTileMobile";
 
 const ServiceSection = () => {
-    const [windowDimensions] = useWindowDimensions();
-
     return (
         <section className="service-options-container">
             <h1>Services</h1>
-            {windowDimensions.width <= 985 ? (
-                <div className="service-options-mobile">
-                    {services.map((service, index) => {
-                        return (
-                            <ServiceTileMobile
-                                key={service.title}
-                                tile={service}
-                                index={index}
-                            />
-                        );
-                    })}
-                </div>
-            ) : (
-                <div className="service-options">
-                    {services.map((service) => {
-                        return (
-                            <ServiceTile key={service.title} tile={service} />
-                        );
-                    })}
-                    <div className="service-overlay"></div>
-                </div>
-            )}
+            <div className="service-options">
+                {services.map((service) => {
+                    return <ServiceTile key={service.title} tile={service} />;
+                })}
+                <div className="service-overlay"></div>
+            </div>
             <div>
                 <p>&#42; Price includes cut &#38; finish (Excludes toner)</p>
                 <p>&#42; Additional £10.00 for long/thick hair</p>
--- src/Components/Pages/ServicePage/Services.jsx
+++ src/Components/Pages/ServicePage/Services.jsx
@@ -3,15 +3,24 @@ import ServiceHeader from "./ServiceHeader";
 import ServiceSection from "./ServiceSection";
 import ServiceTC from "./ServiceTC";
 import MobileCategorySelection from "./MobileCategorySelection";
+import useWindowDimensions from "../../../Hooks/useWindowDimensions";
 
 const Services = () => {
+    const [windowDimensions] = useWindowDimensions();
+
+    const serviceSelect = () => {
+        if (windowDimensions.width > 555) {
+            return <ServiceSection />;
+        }
+        return <MobileCategorySelection />;
+    };
+
     return (
         <main className="services-container">
             <ServiceHeader />
-            <ServiceSection />
+            {serviceSelect()}
             <ServiceTC />
             <ServiceCancellation />
-            <MobileCategorySelection />
         </main>
     );
 };
