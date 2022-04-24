16c68328c1351ca4522f8bcf0c3e493df3a17822
rturner1989 <robturner1000@hotmail.co.uk>
2022-04-24 11:49:52 +0100

    Dynamically map cancellation variable data

--- src/Components/Pages/ServicePage/ServiceCancellation.jsx
+++ src/Components/Pages/ServicePage/ServiceCancellation.jsx
@@ -1,40 +1,22 @@
 import React from "react";
+import { cancellationPolicy } from "../../../Data/Data";
 
 const ServiceCancellation = () => {
     return (
         <section className="cancellation-container">
             <div className="cancellation">
                 <h2>If You Need To Cancel Your Appointment</h2>
-                <div>
-                    <h3>Cancelling An Appointment</h3>
-                    <p>
-                        I know life doesnt always go to plan and things come up,
-                        therefore, in consideration of my other clients,
-                    </p>
-                    <p>
-                        if you cant make an appointment please contact me ASAP.
-                    </p>
-                </div>
-                <div>
-                    <h3>Deposits</h3>
-                    <p>
-                        Less than 24 hours notice will result in a 50%
-                        non-refundable charge of the service you were booked in
-                        for.
-                    </p>
-                    <p>
-                        A no show for the appointment will be a 100%
-                        non-refundable charge of the service.
-                    </p>
-                </div>
-                <div>
-                    <h3>Future Appointments</h3>
-                    <p>
-                        Repeat offenders of no shows will not be welcome to book
-                        any future appointments.
-                    </p>
-                    <p>To cancel, please contact me directly.</p>
-                </div>
+                {cancellationPolicy.map((cancel) => {
+                    const { title, policy } = cancel;
+                    return (
+                        <div key={title}>
+                            <h3>{title}</h3>
+                            {policy.map((para) => {
+                                return <p key={para.slice(0, 9)}>{para}</p>;
+                            })}
+                        </div>
+                    );
+                })}
             </div>
         </section>
     );
